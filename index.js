b1e95bb8399eb14938cc6927efd37224b26d77fa
Mouaz Jamal <mj225ih@student.lnu.se>
2025-11-28 19:32:40 +0100

    Getting the input value and convering to number decimals

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const inputValue = document.getElementById('app_input')
 const button = document.getElementById('app_btn')
+
+button.addEventListener('click', () => {
+    const theValue = parseFloat(inputValue.value)
+    console.log(theValue)
+})
