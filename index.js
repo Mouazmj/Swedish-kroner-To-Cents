2c5dfb9bbb2550115bb8fc7716e8027602d62d48
Mouaz Jamal <mj225ih@student.lnu.se>
2025-11-29 20:59:56 +0100

    Forgot to put the dimes into a varibel

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const inputValue = document.getElementById('app_input')
 const button = document.getElementById('app_btn')
+const result = document.getElementById('result')
+
 
 button.addEventListener('click', () => {
     const theValue = parseFloat(inputValue.value)
     const exchangeRate = 11
+
     const dollars = Math.round(theValue / exchangeRate)
+    
    const totalCents = Math.round(dollars * 100)
+   
    const quarters = Math.floor(totalCents / 25)
    const remainingCents = (totalCents % 25)
-   const nickels = Math.floor(remainingCents / 5)
-   const remainingNickels = (remainingCents % 5)
+
+const dimes = Math.floor(remainingCents / 10)
+const remainingAfterDimes = remainingCents % 10
+
+
+   const nickels = Math.floor(remainingAfterDimes / 5)
+   const remainingNickels = (remainingAfterDimes % 5)
+
    const pennies = remainingNickels
+
+result.innerHTML = `Total cents: ${totalCents}<br>
+Quarters: ${quarters}<br>
+Dimes: `
 })
