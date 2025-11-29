64abfebc318fd8cb6ebcef29ac57b5eafe87f062
Mouaz Jamal <mj225ih@student.lnu.se>
2025-11-29 20:20:06 +0100

    Getting the remainings and and cents quarters nickles and pennies

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const button = document.getElementById('app_btn')
 button.addEventListener('click', () => {
     const theValue = parseFloat(inputValue.value)
     const exchangeRate = 11
-    const krToDollars = Math.round(theValue / exchangeRate)
-    console.log(krToDollars)
+    const dollars = Math.round(theValue / exchangeRate)
+   const totalCents = Math.round(dollars * 100)
+   const quarters = Math.floor(totalCents / 25)
+   const remainingCents = (totalCents % 25)
+   const nickels = Math.floor(remainingCents / 5)
+   const remainingNickels = (remainingCents % 5)
+   const pennies = remainingNickels
 })
